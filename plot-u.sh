f7b6a1228cfc4278ed6f8a6dd1b9fbc8f86ab696
Prof. M.K. Das <72753125+drmanab20@users.noreply.github.com>
2023-12-18 13:55:32 +0530

    Add files via upload

---
+++ plot-u.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+#    set terminal postscript eps color enhanced
+#    set output "OF_vs_EXPT_T$i.eps"
+    set xlabel "u"
+    set ylabel "Channel width, x / [m]"
+    set grid
+    set key left top
+    set size 0.6, 0.6
+    set yrange [0:1]
+    set xrange [-.6:.6]
+    plot \
+        "x0.5.xy" u ($3/.119):($1/0.05) w l, \
+        "u-Ra-5.dat" u 1:2
+
+  
---
+++ plot-v.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+#    set terminal postscript eps color enhanced
+#    set output "OF_vs_EXPT_T$i.eps"
+    set xlabel "Channel width, x / [m]"
+    set ylabel "v"
+    set grid
+    set key left top
+    set size 0.6, 0.6
+    set xrange [0:1]
+    set yrange [-.6:.6]
+    plot \
+        "y0.5.xy" u ($1/0.05):($4/.119) w l, \
+        "v-Ra-5.dat" u 1:2
+
+  
